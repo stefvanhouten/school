2feb7afae8ba6ec2183fd299e1c0f430b1f3ed4e
Stef van Houten <stefvanhouten@gmail.com>
2020-09-10 19:31:15 +0200

    Homework week 2

---
+++ software engineering/Week 2/Opdracht A/Opdracht A/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Opdracht_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("FOR LOOP");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"2^{i} = " + Math.Pow(2, i));
+                }
+            }
+            {
+                Console.WriteLine("\nWHILE LOOP");
+
+                bool doMath = true;
+                int i = 0;
+                while (doMath)
+                {
+                    if(i == 9)
+                    {
+                        doMath = false;
+                    }
+                    Console.WriteLine($"2^{i} = " + Math.Pow(2, i));
+                    i++;
+                }
+            }
+            {
+                Console.WriteLine("\nDO-WHILE LOOP");
+                int i = 0;
+                do
+                {
+                    Console.WriteLine($"2^{i} = " + Math.Pow(2, i));
+                    i++;
+                } while (i < 10);
+            }
+            Console.WriteLine("Give 5 whole numbers: ");
+            {
+                int max, input;
+                max = input = 0;
+                for(int i = 0; i < 5; i++)
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                    if(i == 0)
+                    {
+                        max = input;
+                    }
+                    else
+                    {
+                        if(input > max)
+                        {
+                            max = input;
+                        }
+                    }
+                }
+                Console.WriteLine($"The highest number was: {max}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ software engineering/Week 2/Opdracht B/Opdracht B/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+
+namespace Opdracht_B
+{
+    class Program
+    {
+
+        public static void Fib(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int c = a;
+                a = b; 
+                b = c + b;  
+            }
+
+            Console.WriteLine(a);
+        }
+
+        public static int diceThrow()
+        {
+            Random randomNumber = new Random();
+            int diceRoll = randomNumber.Next(1, 7);
+            return diceRoll;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Fib(i);
+            }
+            Console.ReadKey();
+            int number, multiplier, diceRollOne, diceRollTwo, sixesRolled;
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Choose a number between 5 and 100: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                if(number < 5 || number > 100)
+                {
+                    continue;
+                }
+                break;
+            }
+            multiplier = sixesRolled = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                diceRollOne = diceThrow();
+                diceRollTwo = diceThrow();
+                if(diceRollOne == 6 || diceRollTwo == 6)
+                {
+                    multiplier += 50;
+                    sixesRolled += 1;
+                }
+                if(diceRollOne == diceRollTwo)
+                {
+                    multiplier += 10;
+                }
+                if(diceRollOne == 1 && diceRollTwo == 1)
+                {
+                    multiplier = 0;
+                }
+            }
+            if(sixesRolled >= 4)
+            {
+                multiplier += 2;
+            }
+            Console.Write($"The multiplier: {multiplier}, sixes rolled: {sixesRolled} \n");
+            int profit = multiplier * number;
+            if(profit > number)
+            {
+                Console.WriteLine($"Congratiulations you won {multiplier * number}. Your starting input was {number}");
+            }
+            else
+            {
+                Console.WriteLine($"Oof there goes your money. You recieve {multiplier * number}. Your starting input was {number}");
+            }
+            Console.ReadKey();
+        }   
+
+    }
+}

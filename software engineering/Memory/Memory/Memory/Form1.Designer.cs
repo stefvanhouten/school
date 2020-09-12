9968e6219722d702c7da879873496bfd5eacd877
Stef van Houten <stefvanhouten@gmail.com>
2020-09-12 10:45:42 +0200

    working on a base project for memory

---
+++ software engineering/Memory/Memory/Memory/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new Memory.MyCustomPictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.myCustomPictureBox5 = new Memory.MyCustomPictureBox();
+            this.myCustomPictureBox4 = new Memory.MyCustomPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomPictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomPictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.CustomString = null;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.SlateGray;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(584, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // myCustomPictureBox5
+            // 
+            this.myCustomPictureBox5.CustomString = null;
+            this.myCustomPictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myCustomPictureBox5.Location = new System.Drawing.Point(150, 407);
+            this.myCustomPictureBox5.Name = "myCustomPictureBox5";
+            this.myCustomPictureBox5.Size = new System.Drawing.Size(137, 126);
+            this.myCustomPictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myCustomPictureBox5.TabIndex = 1;
+            this.myCustomPictureBox5.TabStop = false;
+            // 
+            // myCustomPictureBox4
+            // 
+            this.myCustomPictureBox4.CustomString = null;
+            this.myCustomPictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myCustomPictureBox4.Location = new System.Drawing.Point(150, 422);
+            this.myCustomPictureBox4.Name = "myCustomPictureBox4";
+            this.myCustomPictureBox4.Size = new System.Drawing.Size(137, 134);
+            this.myCustomPictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myCustomPictureBox4.TabIndex = 1;
+            this.myCustomPictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomPictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myCustomPictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyCustomPictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MyCustomPictureBox myCustomPictureBox5;
+        private MyCustomPictureBox myCustomPictureBox4;
+    }
+}
+
---
+++ software engineering/Memory/Memory/Memory/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Memory.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddImageToSquares();
+        }
+
+        private void AddImageToSquares()
+        {
+            MyCustomPictureBox pictureBox;
+            var images = new List<Bitmap>(){ Resources.tree, Resources.tree, 
+                                             Resources.book, Resources.book, 
+                                             Resources.banana, Resources.banana, 
+                                             Resources.wine, Resources.wine, 
+                                             Resources.monkey, Resources.monkey 
+                                           };
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if(tableLayoutPanel1.Controls[i] is PictureBox)
+                {
+                    pictureBox = (MyCustomPictureBox)tableLayoutPanel1.Controls[i];
+                }
+                else
+                {
+                    continue;
+                }
+                pictureBox.Visible = true;
+                pictureBox.Image = images[0];
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
